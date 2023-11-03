5086cce24e40ade30d37ed79a0588556c8598068
Matheus <mth123922@gmail.com>
2023-11-03 08:54:47 -0300

    Segundo commit

--- SportSphere.py
+++ SportSphere.py
@@ -1,12 +1,11 @@
-from flask import Flask, request, render_template, redirect, url_for, flash
-from flask_bcrypt import Bcrypt
+import requests
+from flask import Flask, request, jsonify
+from flask_bcrypt import Bcrypt 
 import psycopg2 as pg
 
 app = Flask(__name__)
-app.secret_key = "your_secret_key"  # Defina uma chave secreta para flash messages
 bcrypt = Bcrypt(app)
 
-# Configuração do banco de dados
 conn = pg.connect(
     host="localhost",
     user="postgres",
@@ -15,48 +14,65 @@ conn = pg.connect(
     dbname="SportSphere.bd"
 )
 
-# Rota de registro de usuário
-@app.route('/register', methods=['GET', 'POST'])
+
+@app.route('/register', methods=['POST'])
 def register():
-    if request.method == 'POST':
-        nome = request.form['nome']
-        email = request.form['email']
-        senha = request.form['senha']
+    data = request.get_json()  
 
-        if not nome or not email or not senha:
-            flash('Campos nome, email e senha são obrigatórios', 'error')
-        else:
-            senha_hashed = bcrypt.generate_password_hash(senha).decode('utf-8')
+    nome = data.get('nome')
+    email = data.get('email')
+    senha = data.get('senha')
 
-            cursor = conn.cursor()
-            cursor.execute("INSERT INTO TB_USUARIO (nome, email, senha) VALUES (%s, %s, %s)", (nome, email, senha_hashed))
-            conn.commit()
-            cursor.close()
+    if not nome or not email or not senha:
+        return jsonify({'message': 'Campos nome, email e senha são obrigatórios'}), 400
 
-            flash('Usuário registrado com sucesso', 'success')
-            return redirect(url_for('register'))
+    senha_hashed = bcrypt.generate_password_hash(senha).decode('utf-8')
 
-    return render_template('register.html')
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO TB_USUARIO (nome, email, senha) VALUES (%s, %s, %s)", (nome, email, senha_hashed))
+    conn.commit()
+    cursor.close()
+
+    
+    external_url_register = 'https://sport-sphere-6zl0yjdgv-matheus-silvas-projects-1103c8c3.vercel.app/Cadastro'
+    try:
+        response = requests.get(external_url_register)
+        data = response.json()  
+        
+    except requests.exceptions.RequestException as e:
+        pass
+
+    return jsonify({'message': 'Usuário registrado com sucesso'}), 201
 
 # Rota de login de usuário
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login', methods=['POST'])
 def login():
-    if request.method == 'POST':
-        email = request.form['email']
-        senha = request.form['senha']
+    data = request.get_json()  
 
-        cursor = conn.cursor()
-        cursor.execute("SELECT senha FROM TB_USUARIO WHERE email = %s", (email,))
-        user_data = cursor.fetchone()
-        cursor.close()
+    email = data.get('email')
+    senha = data.get('senha')
 
-        if user_data and bcrypt.check_password_hash(user_data[0], senha):
-            flash('Login bem-sucedido', 'success')
-            return redirect(url_for('login'))
-        else:
-            flash('Credenciais inválidas', 'error')
+    if not email or not senha:
+        return jsonify({'message': 'Campos email e senha são obrigatórios'}), 400
 
-    return render_template('login.html')
+    cursor = conn.cursor()
+    cursor.execute("SELECT senha FROM TB_USUARIO WHERE email = %s", (email,))
+    user_data = cursor.fetchone()
+    cursor.close()
+
+    if user_data and bcrypt.check_password_hash(user_data[0], senha):
+        
+        external_url_login = 'https://sport-sphere-6zl0yjdgv-matheus-silvas-projects-1103c8c3.vercel.app/'
+        try:
+            response = requests.get(external_url_login)
+            data = response.json()  
+            
+        except requests.exceptions.RequestException as e:
+            pass
+
+        return jsonify({'message': 'Login bem-sucedido'}), 200
+    else:
+        return jsonify({'message': 'Credenciais inválidas'}), 401
 
 if __name__ == '__main__':
     app.run(debug=True)
