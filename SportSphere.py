f0f833ad78eda3fd5ae27631bc1392cc92647589
Matheus <mth123922@gmail.com>
2023-11-10 17:31:05 -0300

    sqlite2

--- SportSphere.py
+++ SportSphere.py
@@ -28,14 +28,36 @@ class Cadastro(Base):
     telefone = Column(Integer)
     senha_usuario = Column(String(60))
 
-# Criar a tabela no banco de dados
+
 Base.metadata.create_all(engine)
 
-# Criar uma instância da sessão
+
 Session = sessionmaker(bind=engine)
 session = Session()
 
-@app.route('/', methods=['POST'])
+@app.route('/cadastro', methods=['POST'])
+def cadastro():
+    data = request.get_json()  
+
+    nome = data.get('nome')
+    email = data.get('email')
+    telefone = data.get('telefone')
+    senha = data.get('senha')
+
+    if not nome or not email or not senha or not telefone:
+        return jsonify({'message': 'Campos nome, email, telefone e senha são obrigatórios'}), 400
+
+    
+    senha_hashed = bcrypt.generate_password_hash(senha).decode('utf-8')
+
+   
+    novo_usuario = Cadastro(nome_usuario=nome, email=email,telefone=telefone, senha_usuario=senha_hashed)
+    session.add(novo_usuario)
+    session.commit()
+
+    return jsonify({'message': 'Usuário registrado com sucesso'}), 201
+
+@app.route('/login', methods=['POST'])
 def login():
     data = request.get_json()  
     email = data.get('email')
@@ -44,7 +66,7 @@ def login():
     if not email or not senha:
         return jsonify({'message': 'Campos email e senha são obrigatórios'}), 400
 
-    # Consultar o banco de dados usando SQLAlchemy
+    
     usuario = session.query(Usuario).filter_by(email=email).first()
 
     if usuario and bcrypt.check_password_hash(usuario.senha, senha):
@@ -59,33 +81,7 @@ def login():
     else:
         return jsonify({'message': 'Credenciais inválidas'}), 401   
 
-@app.route('/cadastro', methods=['POST'])
-def cadastro():
-    data = request.get_json()  
 
-    nome = data.get('nome')
-    email = data.get('email')
-    senha = data.get('senha')
-
-    if not nome or not email or not senha:
-        return jsonify({'message': 'Campos nome, email e senha são obrigatórios'}), 400
-
-    # Gerar o hash da senha
-    senha_hashed = bcrypt.generate_password_hash(senha).decode('utf-8')
-
-    # Adicionar usuário ao banco de dados usando SQLAlchemy
-    novo_usuario = Cadastro(nome_usuario=nome, email=email, senha_usuario=senha_hashed)
-    session.add(novo_usuario)
-    session.commit()
-
-    external_url_register = 'https://sport-sphere.vercel.app/Cadastro'
-    try:
-        response = requests.get(external_url_register)
-        data = response.json()  
-    except requests.exceptions.RequestException as e:
-        pass
-
-    return jsonify({'message': 'Usuário registrado com sucesso'}), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
